b30c57347df282a431695e833ac9a03192f6fedc
Ciaran Cumiskey <ciaran.cumiskey@zinkworks.com>
2025-03-05 14:17:24 +0000

    Refactored Account so it can be a JPA entity.

--- src/main/java/com/ciarancumiskey/mockitobank/models/Account.java
+++ src/main/java/com/ciarancumiskey/mockitobank/models/Account.java
@@ -1,5 +1,8 @@
 package com.ciarancumiskey.mockitobank.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
@@ -12,9 +15,10 @@ import static com.ciarancumiskey.mockitobank.utils.Constants.MOCKITO_BANK_IBAN_P
 @Getter
 @Setter
 @Slf4j
+@Entity
 public class Account {
     // accountNumber and sortCode could start with a 0
-    @NonNull String accountNumber;
+    @Id @NonNull String accountNumber;
     @NonNull String sortCode;
     @NonNull String ibanCode;
     @NonNull String accountName;
@@ -31,23 +35,35 @@ public class Account {
         this.overdraftLimit = BigDecimal.ZERO;
     }
 
+    protected Account(){
+        // Required for JPA entity
+    }
+
     public boolean deposit(final BigDecimal depositAmount){
         if(depositAmount.doubleValue() < 0) {
             log.error("You can't deposit a negative amount.");
             return false;
         }
-        this.balance.add(depositAmount);
-        return true;
+        boolean depositSuccessful = adjustBalance(depositAmount);
+        return depositSuccessful;
     }
 
     public boolean withdraw(final BigDecimal withdrawalAmount){
-        BigDecimal withdrawalLimit = balance.add(overdraftLimit);
-        if(withdrawalLimit.doubleValue() < withdrawalAmount.doubleValue()) {
-            log.error("Requested withdrawal amount {} exceeds balance and overdraft limit of {}", withdrawalAmount, withdrawalLimit);
+        if(withdrawalAmount.doubleValue() < 0){
+            log.error("You can't withdraw a negative amount.");
             return false;
-        } else {
-            balance.subtract(withdrawalAmount);
-            return true;
         }
+        final BigDecimal adjustmentAmount = BigDecimal.ZERO.subtract(withdrawalAmount);
+        boolean withdrawalSuccessful = adjustBalance(adjustmentAmount);
+        return withdrawalSuccessful;
+    }
+
+    private boolean adjustBalance(final BigDecimal adjustmentAmount) {
+        if((this.balance.doubleValue() + adjustmentAmount.doubleValue()) < BigDecimal.ZERO.subtract(this.overdraftLimit).doubleValue()){
+            log.error("Withdrawal rejected, your balance would be {} and exceed your overdraft of {}.", (this.balance.doubleValue() + adjustmentAmount.doubleValue()), this.overdraftLimit);
+            return false;
+        }
+        this.balance.add(adjustmentAmount);
+        return true;
     }
 }
